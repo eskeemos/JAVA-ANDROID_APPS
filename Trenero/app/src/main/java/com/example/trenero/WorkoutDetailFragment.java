f06d6d1a403cbb06e0633437e658973b3cd22e8c
Łukasz <l.michalik004@gmail.com>
2021-10-05 16:20:00 +0200

    Add trenero - project(fragments,different screens)

---
+++ Trenero/app/src/main/java/com/example/trenero/WorkoutDetailFragment.java
@@ -0,0 +1,53 @@
+package com.example.trenero;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class WorkoutDetailFragment extends Fragment {
+    private long workoutId;
+
+    public WorkoutDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        if(savedInstanceState != null)
+        {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if(view != null)
+        {
+            TextView nameView = (TextView) view.findViewById(R.id.tvName);
+            TextView descView = (TextView) view.findViewById(R.id.tvDesc);
+
+            Workout workout = Workout.workouts[(int) workoutId];
+
+            nameView.setText(workout.getName());
+            descView.setText(workout.getDesc());
+        }
+    }
+
+    public void setWorkoutId(long id)
+    {
+        this.workoutId = id;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putLong("workoutId", workoutId);
+    }
+}
